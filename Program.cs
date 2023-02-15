57857a9a9fb1bcaaef635b77e256253813d458f4
Vladislav Verin <dippicaka@gmail.com>
2023-02-15 19:55:45 +0300

    added README

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿void ThreeCharOrLess(int n)
+{
+    string[] stringArray = new string[n];
+    Console.WriteLine("Введите элементы массива: ");
+    for (int i = 0; i < n; i++)
+        stringArray[i] = Convert.ToString(Console.ReadLine());
+    Console.WriteLine();
+
+    string[] newStringArray = new string[n];
+    for (int i = 0, j = 0; i < n; i++, j++)
+    {
+        if (stringArray[i].Length <= 3)
+        {
+            newStringArray[j] = stringArray[i];
+            Console.Write(newStringArray[j] + " ");
+        }
+        else j--;
+    };
+}
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ThreeCharOrLess(n);
